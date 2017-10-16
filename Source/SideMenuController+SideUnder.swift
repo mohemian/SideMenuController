65e8ceea317315064a39b8fc2fe0cd96647b2964
Carol Capek <carol.capek@mohemian.com>
2017-10-16 16:08:57 +0200

    Delete unused () in

--- Source/SideMenuController+SideUnder.swift
+++ Source/SideMenuController+SideUnder.swift
@@ -95,7 +95,7 @@ extension SideMenuController {
         UIView.panelAnimation( duration, animations: { 
             self.centerPanel.frame = centerPanelFrame
             self.set(statusUnderlayAlpha: hidden ? 0 : 1)
-        }) { () in
+        }) {
             if hidden {
                 self.setSideShadow(hidden: hidden)
             }
